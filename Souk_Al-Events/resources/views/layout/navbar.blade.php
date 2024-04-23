f6f31493eabc275de19eaab155624f41777bf04c
Mouhcine Essafi <messafi1337@gmail.com>
2024-04-23 11:15:53 +0100

    edit logo navbar

--- Souk_Al-Events/resources/views/layout/navbar.blade.php
+++ Souk_Al-Events/resources/views/layout/navbar.blade.php
@@ -2,10 +2,10 @@
    <div class="container-fluid nav-bar bg-transparent">
        <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
            <a href="{{route('home')}}" class="navbar-brand d-flex align-items-center text-center">
-               <div class="icon p-2 me-2">
-                   <img class="img-fluid" src="{{url('Frontend/img/log.png')}}" alt="Icon" style="width: 30px; height: 30px;">
+               <div class="p-2 me-2">
+                   <img class="img-fluid" src="{{url('Frontend/img/logo-souk-svg_2.png')}}" alt="Icon" style="width: 180px; height: 120px;">
                </div>
-               <h1 class="m-0 text-primary">Evento</h1>
+               {{-- <h1 class="m-0 text-primary">Evento</h1> --}}
            </a>
            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                <span class="navbar-toggler-icon"></span>
